e70095ef04fb5b85618dc109d2f15c6a601cefb0
Daniel <dvijayak@uoguelph.ca>
2012-11-10 21:22:44 -0500

    Increased performance a lot WITH hardware acceleration! Note: drawLine performs much better than drawPath for large n

--- src/ca/uoguelph/cmer/geogryph/PathOverlay.java
+++ src/ca/uoguelph/cmer/geogryph/PathOverlay.java
@@ -18,17 +18,16 @@ public class PathOverlay extends Overlay {
 	private Point pA;
 	private Point pB;
 	
-	private Paint paint;
-	private Path path;
+	private Paint paint;	
 	
 	public PathOverlay (GeoPoint A, GeoPoint B) 
 	{
 		this.A = A;
 		this.B = B;	
-		path = new Path();
+		
 		paint = new Paint();
 		paint.setDither(true);
-		paint.setARGB(100, 205, 0, 205);
+		paint.setARGB(180, 200, 0, 200);
 		paint.setStyle(Paint.Style.FILL_AND_STROKE);
 		paint.setStrokeJoin(Paint.Join.ROUND);
 		paint.setStrokeCap(Paint.Cap.ROUND);
@@ -36,20 +35,13 @@ public class PathOverlay extends Overlay {
 	}
 	
 	public void draw(Canvas canvas, MapView mapView, boolean shadow)
-	{
-		super.draw(canvas, mapView, false);
-						
-		if (shadow != true)			
+	{					
+		if (shadow != false)			
 		{								
 			Projection projection = mapView.getProjection();
 			pA = projection.toPixels(A, null);
 			pB = projection.toPixels(B, null);
-								
-//			path.rewind();
-//			path.moveTo(pB.x, pB.y);
-//			path.lineTo(pA.x, pA.y);
-			
-//			canvas.drawPath(path, paint);
+
 			canvas.drawLine(pA.x, pA.y, pB.x, pB.y, paint);
 		}
 	}
