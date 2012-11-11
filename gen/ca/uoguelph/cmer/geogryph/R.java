53f33273409377206c407575bd15eb03b55ff645
Daniel <dvijayak@uoguelph.ca>
2012-11-11 00:26:02 -0500

    Done for the day

--- gen/ca/uoguelph/cmer/geogryph/R.java
+++ gen/ca/uoguelph/cmer/geogryph/R.java
@@ -60,19 +60,20 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int dialog_about_body=0x7f06000b;
-        public static final int dialog_about_positive=0x7f06000c;
-        public static final int dialog_about_title=0x7f06000a;
-        public static final int dialog_buildings_title=0x7f060009;
-        public static final int menu_about=0x7f060007;
-        public static final int menu_clear=0x7f060005;
-        public static final int menu_empty=0x7f060008;
-        public static final int menu_list=0x7f060006;
-        public static final int menu_plot=0x7f060003;
-        public static final int menu_save=0x7f060004;
-        public static final int menu_search=0x7f060002;
-        public static final int title_activity_main=0x7f060001;
+        public static final int app_name=0x7f060001;
+        public static final int dialog_about_body=0x7f06000c;
+        public static final int dialog_about_positive=0x7f06000d;
+        public static final int dialog_about_title=0x7f06000b;
+        public static final int dialog_buildings_title=0x7f06000a;
+        public static final int maps_api_key=0x7f060000;
+        public static final int menu_about=0x7f060008;
+        public static final int menu_clear=0x7f060006;
+        public static final int menu_empty=0x7f060009;
+        public static final int menu_list=0x7f060007;
+        public static final int menu_plot=0x7f060004;
+        public static final int menu_save=0x7f060005;
+        public static final int menu_search=0x7f060003;
+        public static final int title_activity_main=0x7f060002;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
